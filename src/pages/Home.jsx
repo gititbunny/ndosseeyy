96d52df5dd951852368affc823783507fe7d9aa5
Git It Bunny <gititbunny@gmail.com>
2026-08-10 07:29:47 +0200

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,26 @@
+import "./Home.css";
+
+function Home() {
+  return (
+    <main className="home-page">
+      <section className="home-placeholder">
+        <p className="home-eyebrow">NDOSSEEYY</p>
+
+        <h1>
+          Ideas.
+          <br />
+          Brands.
+          <br />
+          Possibilities.
+        </h1>
+
+        <p className="home-intro">
+          Creative business concepts, brand worlds and thoughtful design for
+          businesses that want to be seen differently.
+        </p>
+      </section>
+    </main>
+  );
+}
+
+export default Home;
