a7f4f30ca28dc1e18d8c57132e97010664e7ae70
Git It Bunny <gititbunny@gmail.com>
2026-08-10 08:51:14 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Concepts from "./pages/Concepts";
+import ConceptDetail from "./pages/ConceptDetail";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/concepts" element={<Concepts />} />
+        <Route path="/concepts/:slug" element={<ConceptDetail />} />
       </Routes>
     </>
   );
