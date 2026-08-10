2674873d15c304d6304fcd18d0aed194308bdacb
Git It Bunny <gititbunny@gmail.com>
2026-08-10 09:43:18 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Concepts from "./pages/Concepts";
 import ConceptDetail from "./pages/ConceptDetail";
 import Services from "./pages/Services";
 import Work from "./pages/Work";
+import Media from "./pages/Media";
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
         <Route path="/concepts/:slug" element={<ConceptDetail />} />
         <Route path="/services" element={<Services />} />
         <Route path="/work" element={<Work />} />
+        <Route path="/media" element={<Media />} />
       </Routes>
     </>
   );
