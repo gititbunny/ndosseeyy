fdb69f8faab65cbd1922f4816dedc89f776a3243
Git It Bunny <gititbunny@gmail.com>
2026-08-14 13:58:45 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { Route, Routes } from "react-router";
-
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import ScrollToTop from "./components/ScrollToTop";
