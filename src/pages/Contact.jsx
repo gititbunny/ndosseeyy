e6ffed0eb6c39767ec9084a4eead733758aeb348
Git It Bunny <gititbunny@gmail.com>
2026-08-14 14:02:40 +0200

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useSearchParams } from "react-router";
+import { useNavigate, useSearchParams } from "react-router";
 import "../styles/Contact.css";
 
 function Contact() {
