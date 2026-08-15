05bd982b4b8c9b399ca4ba4a0ce9c7444a135eff
Git It Bunny <gititbunny@gmail.com>
2026-08-15 18:05:14 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import ThankYou from "./pages/ThankYou";
 import Privacy from "./pages/Privacy";
 import Terms from "./pages/Terms";
 import ScrollReveal from "./components/ScrollReveal";
+import NotFound from "./pages/NotFound";
 
 function App() {
   const location = useLocation();
